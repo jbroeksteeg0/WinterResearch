1f0437493e904f104f75df4e08ebfc5b8117b495
school <jbroeksteeg0@gmail.com>
2023-07-19 09:33:17 +1000

    removed graph class

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -13,38 +13,3 @@ Node::Node(
   this->load = load;
   this->index = index;
 }
-
-Graph::Graph() {}
-
-void Graph::add_node(
-  std::string name,
-  std::pair<int, int> location,
-  std::pair<int, int> times,
-  int unload_time,
-  double load,
-  double bias,
-  int index
-) {
-  m_nodes_[index] = Node(location.first, location.second, times, unload_time, load, bias, index);
-  m_node_names.push_back(name);
-}
-
-void Graph::add_edge(int a, int b, int distance) { m_dist_[a][b] = distance; }
-
-double Graph::get_distance(int a, int b) const { return m_dist_[a][b]; }
-
-double Graph::get_cost(int a, int b) const { return m_dist_[a][b] - m_nodes_[b].bias; }
-
-int Graph::get_node_unpack_time(int name) const { return m_nodes_[name].unload_time; }
-int Graph::get_num_nodes() const { return m_node_names.size(); };
-
-std::pair<int, int> Graph::get_node_times(int name) const { return m_nodes_[name].open_times; }
-
-double Graph::get_node_load(int name) const { return m_nodes_[name].load; }
-
-Node Graph::get_node_data(int name) const {
-  return m_nodes_[name];
-  // assert(m_nodes.find(name) != m_nodes.end());
-  // return m_nodes.find(name)->second;
-};
-std::vector<std::string> Graph::get_node_names() const { return m_node_names; }
--- src/Graph.h
+++ src/Graph.h
@@ -31,36 +31,3 @@ struct Node {
            + std::to_string(unload_time);
   }
 };
-
-class Graph {
-public:
-  Graph();
-
-  void add_node(
-    std::string name,
-    std::pair<int, int> position,
-    std::pair<int, int> times,
-    int unload_time,
-    double load,
-    double bias,
-    int index
-  );
-
-  void add_edge(int a, int b, int distance);
-
-  double get_distance(int a, int b) const;
-  double get_cost(int a, int b) const;
-  int get_num_nodes() const;
-  int get_node_unpack_time(int name) const;
-  double get_node_load(int name) const;
-  std::pair<int, int> get_node_times(int name) const;
-  std::vector<std::string> get_node_names() const;
-  int get_node_ind(int name) const;
-  Node get_node_data(int name) const;
-
-  std::array<Node, NUM_NODES + 1> m_nodes_;
-  std::array<std::array<int, NUM_NODES + 1>, NUM_NODES + 1> m_dist_;
-
-private:
-  std::vector<std::string> m_node_names;
-};
